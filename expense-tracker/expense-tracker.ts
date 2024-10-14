592163d8f22123a4b1e1d6d74a6e566f3ef1ec86
Dineshwar <29286641+dineshwar@users.noreply.github.com>
2024-10-15 00:14:15 +0530

    Update the logic for commander

--- expense-tracker/expense-tracker.ts
+++ expense-tracker/expense-tracker.ts
@@ -4,11 +4,20 @@ const { Command } = require('commander');
 
 const expenseTrackerCli = new Command();
 
+interface AddOptions  {
+  description: string;
+  amount: number;
+}
+
 expenseTrackerCli
   .version('1.0.0')
   .description('An expense tracker cli')
   .command('add')
   .requiredOption('--description <description>', 'Description of the expense')
-  .requiredOption('--amount <amount>', 'Amount of the expense');
+  .requiredOption('--amount <amount>', 'Amount of the expense')
+  .action((options: AddOptions) => {
+    const { description, amount } = options;
+    console.log(`${description} ${amount}`)
+  });
 
 expenseTrackerCli.parse();
