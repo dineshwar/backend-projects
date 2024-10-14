edb23d793e9283d94443a125cd54618e63db281e
Dineshwar <29286641+dineshwar@users.noreply.github.com>
2024-10-14 23:53:09 +0530

    Update the logic for commander for add

--- expense-tracker/expense-tracker.ts
+++ expense-tracker/expense-tracker.ts
@@ -1,11 +1,14 @@
 #!/usr/bin/env node
 
-const { Command } = require("commander");
+const { Command } = require('commander');
 
 const expenseTrackerCli = new Command();
 
 expenseTrackerCli
-  .version("1.0.0")
-  .description("An expense tracker cli");
+  .version('1.0.0')
+  .description('An expense tracker cli')
+  .command('add')
+  .requiredOption('--description <description>', 'Description of the expense')
+  .requiredOption('--amount <amount>', 'Amount of the expense');
 
 expenseTrackerCli.parse();
