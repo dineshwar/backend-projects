d37a1db21dcc208fe81be2bb991b674369daac22
Dineshwar <29286641+dineshwar@users.noreply.github.com>
2024-10-14 23:16:36 +0530

    Update expense tracker

--- expense-tracker/expense-tracker.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env node
-const args = process.argv;
-const myArgs = args.slice(2);
-
-console.log(myArgs)
---
+++ expense-tracker/expense-tracker.ts
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+
+const { Command } = require("commander");
+
+const expenseTrackerCli = new Command();
+
+expenseTrackerCli
+  .version("1.0.0")
+  .description("An expense tracker cli");
