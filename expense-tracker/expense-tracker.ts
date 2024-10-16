8cb4f3f6863d7394cb957b1f506c89bc757d531e
Dineshwar <29286641+dineshwar@users.noreply.github.com>
2024-10-16 21:12:04 +0530

    Added all the command

--- expense-tracker/expense-tracker.ts
+++ expense-tracker/expense-tracker.ts
@@ -11,13 +11,30 @@ interface AddOptions  {
 
 expenseTrackerCli
   .version('1.0.0')
-  .description('An expense tracker cli')
-  .command('add')
+  .description('An expense tracker cli');
+// Add command 
+  expenseTrackerCli.command('add')
   .requiredOption('--description <description>', 'Description of the expense')
   .requiredOption('--amount <amount>', 'Amount of the expense')
   .action((options: AddOptions) => {
     const { description, amount } = options;
     console.log(`${description} ${amount}`)
   });
+  
+  expenseTrackerCli.command('list')
+    .action(() => {
+
+  })
+
+  expenseTrackerCli.command('summary')
+    .option('--month').action((options) => {
+
+  })
+  
+  expenseTrackerCli.command('delete')
+    .requiredOption('--id')
+    .action((options) => {
+
+  })
 
 expenseTrackerCli.parse();
