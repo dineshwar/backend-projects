c96a560375933a1674c584e24d6e3caf682c0de4
Dineshwar <29286641+dineshwar@users.noreply.github.com>
2024-11-20 00:24:37 +0530

    Added some logic for home route

--- personal-blog/index.ts
+++ personal-blog/index.ts
@@ -1,13 +1,27 @@
-import express from 'express';
+import express, {Request, Response} from 'express';
+import fs from 'node:fs';
 
 const app = express();
-
+const articlesDir = "./articles/"
 app.set('view engine', 'ejs');
 
+const homeAction = (req: Request, res: Response) => {
+  const files = fs.readdirSync(articlesDir);
+  const articleList = [];
+  files.forEach(file => {
+    
+    const fileCon = fs.readFileSync(articlesDir+file, 'utf8');
+    articleList.push(JSON.parse(fileCon))
+  });
+  console.log(articleList);
 
-app.get('/home', function(req, res) {
-  res.render('pages/index');
-});
+  res.render('pages/home', {
+    articleList
+  });
+}
+
+app.get('/', homeAction);
+app.get('/home', homeAction);
 
 
 app.listen(8080);
