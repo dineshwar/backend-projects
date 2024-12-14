52b6f0a3ec0d298939c483e183befecdf5b9d34d
Dineshwar <29286641+dineshwar@users.noreply.github.com>
2024-12-14 19:09:13 +0530

    Update the article page

--- personal-blog/index.ts
+++ personal-blog/index.ts
@@ -5,24 +5,50 @@ const app = express();
 const articlesDir = "./articles/"
 app.set('view engine', 'ejs');
 
+
+
 const homeAction = (req: Request, res: Response) => {
   const files = fs.readdirSync(articlesDir);
   const articleList = [];
   files.forEach(file => {
     
     const fileCon = fs.readFileSync(articlesDir+file, 'utf8');
-    articleList.push(JSON.parse(fileCon))
+    let fileContent = JSON.parse(fileCon);
+    fileContent.fileName = file.replace('.json', '');
+    articleList.push(fileContent);
   });
-  console.log(articleList);
 
   res.render('pages/home', {
     articleList
   });
 }
 
+const articleAction = (req: Request, res: Response) => {
+  const files = fs.readdirSync(articlesDir);
+  let fileInfo = {};
+  const reqFile = req.params.id;
+  files.forEach(file => {
+    if (`${reqFile}.json` === file) {
+      fileInfo = JSON.parse(fs.readFileSync(`${articlesDir}${file}`, 'utf8'));
+    }
+  });
+  
+  if (JSON.stringify(fileInfo) == '{}') {
+    res.status(404).render('pages/404')
+  } else {
+    res.render('pages/article', {
+      fileInfo
+    })
+  }
+}
+
 app.get('/', homeAction);
 app.get('/home', homeAction);
+app.get('/article/:id', articleAction);
 
+app.use(function(req, res, next) {
+  res.status(404).render('pages/404')
+})
 
 app.listen(8080);
 console.log('Server is listening on port 8080');
