700b74abad71a060a9dab4e2b5033eb5700d7ae9
Dineshwar <29286641+dineshwar@users.noreply.github.com>
2024-12-20 01:06:11 +0530

    Added redis test connect and set

--- weather-api-wrapper-service/main.ts
+++ weather-api-wrapper-service/main.ts
@@ -1,10 +1,12 @@
-import { serveDir } from "@std/http";
+import { createClient } from 'npm:redis';
 
 export default {
-  fetch(req) {
+  async fetch(req) {
     const url = new URL(req.url);
-    const redis = '';
-
+    const client = await createClient()
+      .on('error', err => console.log('Redis Client Error',err)).connect();
+    await client.set('testkey', 'testvalue')
+    client.disconnect();
     if (url.pathname === "/") {
       return Response.json({
         weather: "Rainy",
